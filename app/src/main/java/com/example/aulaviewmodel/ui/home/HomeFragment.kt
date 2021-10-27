4518deec3c184bb6df5a66d5d05ab495f38c258d
Eric Roberto <58601124+ericDevRoberto@users.noreply.github.com>
2021-10-27 18:34:34 -0300

    MVVM

--- app/src/main/java/com/example/aulaviewmodel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/aulaviewmodel/ui/home/HomeFragment.kt
@@ -8,14 +8,20 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.aulaviewmodel.R
 import com.example.aulaviewmodel.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+
+    //Instanciando o viewModel e o binding
     private lateinit var viewModel: HomeViewModel
     private lateinit var binding: FragmentHomeBinding
 
+
+    //Iniciando o bindind de maneira rapida e eficiente
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,24 +29,27 @@ class HomeFragment : Fragment() {
     ): View = FragmentHomeBinding.inflate(inflater, container, false).apply {
         binding = this
         init()
-        setListenners()
-        setObservers()
     }.root
 
+    // Iniciador da View
     private fun init() {
         viewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        setListenners()
+        setObservers()
     }
 
+    //observador
     private fun setObservers(){
         viewModel.action.observe(viewLifecycleOwner){ action ->
             when(action){
                 is HomeViewModel.Action.ChangeTextName -> onSetText(action.text)
+                HomeViewModel.Action.goToNotification -> openNotification()
             }
         }
     }
 
+    // Iniciando Listeners
     private fun setListenners() {
         with(binding){
             homeButtonFirst.setOnClickListener {
@@ -55,7 +64,12 @@ class HomeFragment : Fragment() {
         }
     }
 
+    //Função para a View
     private fun onSetText(text:String){
         binding.homeTextViewText.text = text
+    }
+
+    private fun openNotification(){
+        //findNavController().navigate(NOTIFICATION)
     }
 }
--- app/src/main/java/com/example/aulaviewmodel/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/aulaviewmodel/ui/home/HomeViewModel.kt
@@ -1,18 +1,24 @@
 package com.example.aulaviewmodel.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
+
     internal sealed class Action{
         data class ChangeTextName(val text: String) :Action()
+        object goToNotification : Action()
     }
 
     internal val action = MutableLiveData<Action>()
 
+
     fun onTakeText(text: String){
         action.postValue(Action.ChangeTextName(text))
+    }
+
+    fun directToNotification(){
+        action.postValue(Action.goToNotification)
     }
 }
